a49554d7ee205c6e07b79d6ad8c8806e4ecf073a
Tero Lindeman <kometbomb@kometbomb.net>
2017-08-13 11:04:56 +0300

    Reset inputs/outputs on SynthModule construction

--- src/SynthModule.cpp
+++ src/SynthModule.cpp
@@ -3,6 +3,11 @@
 SynthModule::SynthModule(ModularSynth& synth, int synthId, int numInputs, int numOutputs, int numParams)
 	: mSynth(synth), mSynthId(synthId), mNumInputs(numInputs), mNumOutputs(numOutputs), mNumParams(numParams)
 {
+	for (int i = 0 ; i < mNumInputs ; ++i)
+		mInputs[i] = 0.0f;
+	
+	for (int i = 0 ; i < mNumOutputs ; ++i)
+		mOutputs[i] = 0.0f;
 }
 
 
