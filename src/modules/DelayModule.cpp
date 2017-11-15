09f25ff342a45e5baa208b34e26a80e462be1439
Tero Lindeman <kometbomb@kometbomb.net>
2017-11-15 20:24:34 +0200

    Rewrote DelayModule cycle

--- src/modules/DelayModule.cpp
+++ src/modules/DelayModule.cpp
@@ -21,27 +21,40 @@ void DelayModule::cycle()
 {
 	if (mBuffer == NULL)
 		return;
-	
+
 	mBuffer[mHead] = getInput(0);
-	
+
+	int currentReadHead = static_cast<int>(getInput(1) * mSampleRate);
+	int a = std::min(currentReadHead, mPrevReadHead);
+	int b = std::max(currentReadHead, mPrevReadHead);
+	float avg = 0;
+
+	for (int i = a ; i <= b ; ++i)
+	{
+		int readHead = (mHead - i + mMaxBufferSize) % mMaxBufferSize;
+		avg += mBuffer[readHead];
+	}
+
+	setOutput(0, avg / (b - a + 1));
+
 	++mHead;
-	
-	if (mHead >= std::max(0, std::min(mMaxBufferSize, static_cast<int>(getInput(1) * mSampleRate))))
+
+	if (mHead >= mMaxBufferSize)
 		mHead = 0;
-	
-	setOutput(0, mBuffer[mHead]);
+
+	mPrevReadHead = currentReadHead;
 }
 
 
 
-const char * DelayModule::getInputName(int input) const 
+const char * DelayModule::getInputName(int input) const
 {
 	static const char *names[] = {"Input", "Delay"};
 	return names[input];
 }
 
 
-const char * DelayModule::getOutputName(int output) const 
+const char * DelayModule::getOutputName(int output) const
 {
 	return "Output";
 }
@@ -61,14 +74,16 @@ SynthModule * DelayModule::createModule(ModularSynth& synth)
 void DelayModule::setSampleRate(int newRate)
 {
 	SynthModule::setSampleRate(newRate);
-	
+
 	if (mBuffer != NULL)
 		delete[] mBuffer;
-	
-	mMaxBufferSize = maxBufferSizeMs * newRate / 1000;
-	
+
+	mMaxBufferSize = std::max(1, maxBufferSizeMs * newRate / 1000);
+
 	mBuffer = new float[mMaxBufferSize];
 	SDL_memset(mBuffer, 0, mMaxBufferSize * sizeof(mBuffer[0]));
-	
+
+	mPrevReadHead = static_cast<int>(getInput(1) * mSampleRate);
+
 	mHead = 0;
 }
