38ff00a2506849a0f736bbfe26740cbfbe83f0b8
Tero Lindeman <kometbomb@kometbomb.net>
2018-03-12 20:42:21 +0200

    Move multiple nodes with ALT

--- src/AutomationEditor.cpp
+++ src/AutomationEditor.cpp
@@ -42,7 +42,7 @@ SDL_Rect AutomationEditor::getNodeArea(const SDL_Rect& parent, const Synth::Auto
 {
 	const int nodeWidth = 5;
 	const int nodeHeight = 5;
-	
+
 	SDL_Rect area = {
 		node.time + parent.x - nodeWidth / 2 - getScrollPosition(),
 		static_cast<int>((1.0f - node.value) * parent.h) + parent.y - nodeHeight / 2,
@@ -70,7 +70,7 @@ void AutomationEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 
 	for (int x = area.x - getScrollPosition() ; x < area.w + area.x ; x += patternLength)
 	{
-		renderer.renderLine(x, area.y, x, area.y + area.h, Color(64,64,64));	
+		renderer.renderLine(x, area.y, x, area.y + area.h, Color(64,64,64));
 	}
 
 	float time = synth.getSongPosition();
@@ -78,7 +78,7 @@ void AutomationEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 
 	renderer.renderLine(time - getScrollPosition() + area.x, area.y, time - getScrollPosition() + area.x, area.y + area.h, Color(0,0,128));
 	renderer.renderLine(editPos - getScrollPosition() + area.x, area.y, editPos - getScrollPosition() + area.x, area.y + area.h, Color(255,255,255));
-	
+
 	for (int track = 0 ; track < Synth::maxAutomationTracks ; ++track)
 	{
 		if (track == mSelectedTrack)
@@ -91,13 +91,13 @@ void AutomationEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 			SDL_Rect nodeArea = getNodeArea(area, synth.getAutomationTrack(track).nodes[0]);
 			int px = nodeArea.x + nodeArea.w / 2;
 			int py = nodeArea.y + nodeArea.h / 2;
-			
+
 			for (int i = 1 ; i < synth.getAutomationTrack(track).numNodes ; ++i)
 			{
 				SDL_Rect nodeArea = getNodeArea(area, synth.getAutomationTrack(track).nodes[i]);
 				int x = nodeArea.x + nodeArea.w / 2;
 				int y = nodeArea.y + nodeArea.h / 2;
-				
+
 				renderer.renderLine(px, py, x, y, track == mSelectedTrack ? Color(255, 255, 255) : Color(64, 64, 64));
 
 				px = x;
@@ -112,13 +112,13 @@ void AutomationEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 		SDL_Rect nodeArea = getNodeArea(area, 0);
 		int px = nodeArea.x + nodeArea.w / 2;
 		int py = nodeArea.y + nodeArea.h / 2;
-		
+
 		for (int i = 1 ; i < getAutomationTrack().numNodes ; ++i)
 		{
 			SDL_Rect nodeArea = getNodeArea(area, i);
 			int x = nodeArea.x + nodeArea.w / 2;
 			int y = nodeArea.y + nodeArea.h / 2;
-			
+
 			renderer.renderLine(px, py, x, y, Color(255, 255, 255));
 
 			px = x;
@@ -131,7 +131,7 @@ void AutomationEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 			renderer.clearRect(nodeArea, mSelectedNode == i ? Color(255, 255, 255) : Color(255, 0, 0));
 		}
 	}
-	
+
 	const SDL_Rect textArea = { area.x + area.w - renderer.getFontWidth() * 2 - 1, area.y + 1, 16, 16 };
 	renderer.renderTextV(textArea, Color(192, 140, 32), "%02d", mSelectedTrack);
 }
@@ -140,7 +140,7 @@ void AutomationEditor::onDraw(Renderer& renderer, const SDL_Rect& area)
 bool AutomationEditor::pickNode(int x, int y, const SDL_Rect& area, int& nodeOut) const
 {
 	SDL_Point point = {x,y};
-	
+
 	for (int i = 0 ; i < getAutomationTrack().numNodes ; ++i)
 	{
 		if (pointInRect(point, getNodeArea(area, i)))
@@ -163,16 +163,17 @@ bool AutomationEditor::onEvent(SDL_Event& event)
 
 		mMouseX = event.button.x / SCALE;
 		mMouseY = event.button.y / SCALE;
-		
+
 		switch (event.button.button)
 		{
 			case SDL_BUTTON_LEFT:
 				if (event.button.clicks == 1)
 				{
-					int node;
-					if (pickNode(mMouseX, mMouseY, mThisArea, node))
+					int nodeIdx;
+					if (pickNode(mMouseX, mMouseY, mThisArea, nodeIdx))
 					{
-						mSelectedNode = node;
+						Synth::AutomationNode& node = getAutomationTrack().getNode(nodeIdx);
+						mSelectedNode = nodeIdx;
 					}
 					else
 					{
@@ -180,12 +181,12 @@ bool AutomationEditor::onEvent(SDL_Event& event)
 					}
 					setDirty(true);
 				}
-				else if (event.button.clicks == 2) 
+				else if (event.button.clicks == 2)
 				{
 					Synth& synth = static_cast<Synth&>(mSynth);
 					SDL_Keymod modState = SDL_GetModState();
 					int xGranularity = 1, yGranularity = 1;
-					
+
 					if (modState & KMOD_CTRL)
 					{
 						xGranularity = synth.getPatternLength();
@@ -193,7 +194,7 @@ bool AutomationEditor::onEvent(SDL_Event& event)
 					}
 
 					mSelectedNode = getAutomationTrack().addNode(
-						granularize(mMouseX - mThisArea.x + getScrollPosition(), xGranularity), 
+						granularize(mMouseX - mThisArea.x + getScrollPosition(), xGranularity),
 						1.0f - static_cast<float>(granularize(mMouseY - mThisArea.y, yGranularity)) / mThisArea.h
 					);
 					setDirty(true);
@@ -203,27 +204,51 @@ bool AutomationEditor::onEvent(SDL_Event& event)
 
 		return false;
 	}
-	else if (event.type == SDL_MOUSEMOTION) 
+	else if (event.type == SDL_MOUSEMOTION)
 	{
 		mMouseX = event.motion.x / SCALE;
 		mMouseY = event.motion.y / SCALE;
-		
+
 		if ((event.motion.state & SDL_BUTTON(SDL_BUTTON_LEFT)) && mSelectedNode != -1)
 		{
 			Synth& synth = static_cast<Synth&>(mSynth);
 			SDL_Keymod modState = SDL_GetModState();
 			int xGranularity = 1, yGranularity = 1;
-			
+			Synth::AutomationTrack& track = getAutomationTrack();
+			Synth::AutomationNode& firstNode = track.getNode(mSelectedNode);
+			int origTime = firstNode.time;
+			float origValue = firstNode.value;
+
 			if (modState & KMOD_CTRL)
 			{
 				xGranularity = synth.getPatternLength() / 2;
 				yGranularity = mThisArea.h / 2;
 			}
 
-			getAutomationTrack().editNode(mSelectedNode,
+			track.editNode(mSelectedNode,
 				granularize(mMouseX - mThisArea.x + getScrollPosition(), xGranularity),
 				1.0f - static_cast<float>(granularize(mMouseY - mThisArea.y, yGranularity)) / mThisArea.h
 			);
+
+			if (modState & KMOD_ALT)
+			{
+
+				int deltaTime = firstNode.time - origTime;
+				float deltaValue = firstNode.value - origValue;
+
+				for (int nodeIdx = mSelectedNode + 1 ; nodeIdx < track.numNodes ; ++nodeIdx)
+				{
+					Synth::AutomationNode node = track.getNode(nodeIdx);
+					track.editNode(nodeIdx,
+						node.time + deltaTime,
+						node.value + deltaValue
+					);
+				}
+			}
+			else
+			{
+
+			}
 		}
 	}
 	else if (event.type == SDL_MOUSEWHEEL)
--- src/AutomationEditor.h
+++ src/AutomationEditor.h
@@ -11,7 +11,7 @@ class AutomationEditor: public Editor
 private:
 	ISynth& mSynth;
 	int mSelectedNode, mSelectedTrack, mMouseX, mMouseY;
-	
+
 	int getScrollPosition() const;
 	bool pickNode(int x, int y, const SDL_Rect& area, int& nodeOut) const;
 	Synth& getSynth();
@@ -23,10 +23,10 @@ private:
 	SDL_Rect getNodeArea(const SDL_Rect& parent, const Synth::AutomationNode& node) const;
 	virtual void onDraw(Renderer& renderer, const SDL_Rect& area);
 public:
-	
+
 	AutomationEditor(EditorState& editorState, ISynth& synth, IPlayer& player);
 	virtual ~AutomationEditor();
-	
+
 	virtual bool onEvent(SDL_Event& event);
 	virtual void onListenableChange(Listenable *listenable);
 	virtual void onLoaded();
