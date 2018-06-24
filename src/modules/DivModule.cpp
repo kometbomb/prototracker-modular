212fd271dd8dc12923a14dba9dbe63e86577077a
Tero Lindeman <kometbomb@kometbomb.net>
2018-06-24 19:04:49 +0300

    Added DivModule

---
+++ src/modules/DivModule.cpp
@@ -0,0 +1,40 @@
+#include "DivModule.h"
+#include <algorithm>
+
+DivModule::DivModule(ModularSynth& synth)
+	:SynthModule(synth, moduleId, 1, 1, 0)
+{
+}
+
+
+void DivModule::cycle()
+{
+	float divisor = std::max(1.0f, getInput(1));
+	setOutput(0, getInput(0) / divisor);
+}
+
+
+const char * DivModule::getInputName(int input) const
+{
+	static const char *names[] = {"Input", "Div"};
+	return names[input];
+}
+
+
+const char * DivModule::getOutputName(int output) const
+{
+	static const char *names[] = {"Output"};
+	return names[output];
+}
+
+
+const char * DivModule::getName() const
+{
+	return "Div";
+}
+
+
+SynthModule * DivModule::createModule(ModularSynth& synth)
+{
+	return new DivModule(synth);
+}
